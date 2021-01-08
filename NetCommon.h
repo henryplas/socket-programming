a8b9c1f40d9e3ed4fb806bc7f6ec08d3a8ac6b7c
henryplas <43822725+henryplas@users.noreply.github.com>
2021-01-07 19:43:46 -0500

    Add files via upload

---
+++ NetCommon.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <deque>
+#include <optional>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0X0A00
+#endif
+
+#include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+#include <chrono>
+
+using namespace boost;
+
+
+
+
